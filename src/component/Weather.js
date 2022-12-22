5b8802d7aa3d6d466c80680362cc2918822316f3
Bhumi Patel <bhumipatel1196@gmail.com>
2022-12-21 21:24:08 -0500

    config open weather api and display temparature of searched city

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,7 +1,34 @@
-import React, { useState } from "react";
-import '../App.css';
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import axios from "axios";
+import {apiKey} from "../api";
+
 
 const Weather = () => {  
+    const [inputCity, setInputCity] = useState("")
+    const [data, setData] = useState({})
+  
+  
+    const getWetherDetails = (cityName) => {
+      if (!cityName) return
+      const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+      axios.get(apiURL).then((res) => {
+        console.log("response", res.data)
+        setData(res.data)
+      }).catch((err) => {
+        console.log("err", err)
+      })
+    }
+  
+    const handleChangeInput = (e) => {
+      console.log("value", e.target.value)
+      setInputCity(e.target.value)
+    }
+  
+    const handleSearch = () => {
+      getWetherDetails(inputCity)
+    }
+  
   
     return (
       <div className="col-md-12">
@@ -10,13 +37,15 @@ const Weather = () => {
   
           <div className="d-grid gap-3 col-4 mt-4">
             <input type="text" className="form-control"
-             />
+              value={inputCity}
+              onChange={handleChangeInput} />
             <button className="btn btn-primary" type="button"
+              onClick={handleSearch}
             >Search</button>
           </div>
         </div>
   
-        {data && Object.keys(data).length > 0 &&
+        {Object.keys(data).length > 0 &&
           <div className="col-md-12 text-center mt-5">
   
             <div className="shadow rounded wetherResultBox">
